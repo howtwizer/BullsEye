224e53ed99d66c50be021a4c49c29737c4169bd6
Alex Baidan <howtwizer@gmail.com>
2018-12-18 09:02:28 +0100

    Initial Commit

---
+++ BullsaEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsaEye
+//
+//  Created by Oleksii Baidan on 12/18/18.
+//  Copyright © 2018 Oleksii Baidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
